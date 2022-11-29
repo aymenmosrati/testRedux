b178eb629dae69866b9189bf8297e9dbc42033d6
Taki Academy <takiacademy@Takis-Mac-mini.local>
2022-11-29 21:12:22 +0100

    create action reducers redux

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,14 +2,45 @@ import "../App.css";
 import { useSelector, useDispatch } from "react-redux";
 import { increase, decrease } from "../store/counterSlice";
 import { logIn, logOut } from "../store/authSlice";
+import { useCallback, useEffect } from "react";
 
 const Counter = () => {
   //   const globalState = useSelector((state) => {
   //     console.log(state);
   //     return state;
   //   });
+
+  //useEffect ->2
+  //-> dispatch
+  //re render
+
+  //useEffect -> 3
+  //-> dispatch
+  // re evalute -> 4
+  //re render
+
+  //useEffect -> 4
+  //re evalute -> ref -> 1 /ref -> 2
+  //re evalute -> counterHnadler -> 3
+
   const globalState = useSelector((state) => state);
   const dispatch = useDispatch();
+
+  const counterHandler = useCallback(
+    (type, value) => {
+      if (type === "increase") {
+        dispatch(increase(value));
+      } else {
+        dispatch(decrease(value));
+      }
+    },
+    [dispatch]
+  );
+
+  useEffect(() => {
+    // dispatch(increase(20));
+    counterHandler("increase", 20);
+  }, [counterHandler]);
 
   const isLoggedIn = () => {
     return globalState.auth.isLoggedIn;
@@ -17,7 +48,7 @@ const Counter = () => {
 
   const loginHandler = (status) => {
     if (status) {
-      dispatch(logOut());
+      dispatch(logOut(5));
     } else {
       dispatch(logIn());
     }
@@ -30,10 +61,16 @@ const Counter = () => {
         <>
           <div className="counter">Counter: {globalState.counter.value}</div>;
           <div>
-            <button className="btn" onClick={() => dispatch(increase(5))}>
+            <button
+              className="btn"
+              onClick={() => counterHandler("increase", 5)}
+            >
               increase +
             </button>
-            <button className="btn" onClick={() => dispatch(decrease(2))}>
+            <button
+              className="btn"
+              onClick={() => counterHandler("decrease", 2)}
+            >
               decrease -
             </button>
           </div>
